247af2340255d7a0823172f9f09c586fff909fe0
barasapeter <barasapeter52@gmail.com>
2026-03-05 19:03:53 +0300

    Shop view

--- apps/web/main.py
+++ apps/web/main.py
@@ -3,15 +3,13 @@ import traceback
 
 from fastapi import APIRouter, Request, Depends
 from fastapi.templating import Jinja2Templates
-from fastapi.responses import RedirectResponse
+from fastapi.responses import RedirectResponse, Response
 
 from data.models import ShopOwner, Employee, Shop
 
 from core import config
 
-from core.security import (
-    get_current_user_optional,
-)
+from core.security import get_current_user_optional, clear_auth_cookies
 
 router = APIRouter()
 
@@ -39,6 +37,12 @@ async def main(request: Request):
     return templates.TemplateResponse("login.html", context)
 
 
+@router.get("/logout")
+async def main(response: Response):
+    clear_auth_cookies(response)
+    return RedirectResponse("/login")
+
+
 @router.get("/dashboard")
 async def main(request: Request, user=Depends(get_current_user_optional)):
     context = {"request": request, "user": user}
@@ -46,11 +50,11 @@ async def main(request: Request, user=Depends(get_current_user_optional)):
         if user is None:
             return RedirectResponse(url="/login")
 
-        # for owners only. 
         if isinstance(user, ShopOwner):
             context["user"] = user
             context["shops"] = user.shops
         elif isinstance(user, Employee):
+            request.session["shop_id"] = user.shop.id
             return RedirectResponse("/shop")
 
         return templates.TemplateResponse("dashboard.html", context)

d9d1d2cb02e8cd79e7adb2d391b1275b32372d76
barasa <101450156+barasapeter@users.noreply.github.com>
2026-05-05 11:32:23 +0300

    Update speedy domain to eu.cardlabs.cloud

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-DOMAIN="speedy.cardlabs.cloud"
+DOMAIN="eu.cardlabs.cloud"
 EMAIL="barasapeter52@gmail.com"
 REPO="https://github.com/barasapeter/BIKELEASE.git"
 APP_DIR="/home/ubuntu/BIKELEASE"
